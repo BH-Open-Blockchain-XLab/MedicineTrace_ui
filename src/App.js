8e349e8e8f5b4f77a6066fef8c4a35297a5bf2de
Itachi666 <494521900@qq.com>
2018-12-05 13:05:49 +0800

    mission 1 complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Link, withRouter} from 'react-router-dom'
+import Notifications from 'react-notify-toast'
+
+
+import {
+    Collapse,
+    Navbar,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+} from 'reactstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    render() {
+        const bodyColor = "hsl(136.7, 25%, 98.1%)";
+
+        const appJSX = (
+            <div style={{
+                minHeight: "100vh",
+                borderTop: "4px solid #50b796",
+                backgroundColor: "black",
+                fontFamily: "Barlow"
+            }}>
+                <Notifications/>
+                <Navbar color="faded" light
+                        style={{paddingTop: "1em", paddingBottom: "2em", backgroundColor: bodyColor}} expand="md">
+                    <Container>
+                        <Link to='/'><img alt="Logo Trace" style={{width: "260px", marginRight: "20px"}}
+                                          src={require("./pic/logo-black.svg")} /></Link>
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/account">
+                                        My account
+                                        <img alt="Profile avatar" src={require('./pic/truth.png')} style={{
+                                            marginLeft: "10px",
+                                            width: "25px",
+                                            height: "25px",
+                                            borderRadius: "3px"
+                                        }}/>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+                <div style={{backgroundColor: bodyColor, paddingBottom: "3em"}}>
+                    <Container>
+                        {this.props.children}
+                    </Container>
+                </div>
+                <div style={{padding: "2em 0", color: "white", backgroundColor: "#000000"}}>
+                    <Container>
+                        © Medicine Trace
+                    </Container>
+                </div>
+            </div>
+        );
+
+        return (
+            <div>
+                {appJSX}
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/pages/Account.js
@@ -0,0 +1,125 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux';
+
+import AnnotatedSection from '../components/AnnotatedSection'
+import UpdateGodUser from '../components/UpdateGodUser';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import faUser from '@fortawesome/fontawesome-free-solid/faUser'
+import faCertificate from '@fortawesome/fontawesome-free-solid/faCertificate'
+
+class Account extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isGod: false,
+            availableCertifications: ''
+        };
+
+        // props.web3.eth.getBalance(props.web3Accounts[0], function (err, result) {
+        //     document.getElementById("EtherBalance").innerHTML = props.web3.fromWei(result, 'ether');
+        // });
+    }
+
+    /*componentDidMount() {
+        this.props.passageInstance.isUserGod({from: this.props.web3Accounts[0]})
+            .then((result) => {
+                this.setState({isGod: result});
+            });
+
+        this.props.passageInstance.getActorCertificationsIds({from: this.props.web3Accounts[0]})
+            .then((result) => {
+                result.map((certificationId) => {
+                    return this.props.passageInstance.getCertificationById(String(certificationId).valueOf())
+                        .then((result) => {
+                            const certification = {
+                                name: result[0],
+                                imageUrl: result[1],
+                                id: certificationId,
+                            }
+                            return this.setState({availableCertifications: [...this.state.availableCertifications, certification]})
+                        });
+                });
+            })
+    }*/
+
+    render() {
+        return (
+            <div>
+                <AnnotatedSection
+                    annotationContent={
+                        <div>
+                            <FontAwesomeIcon fixedWidth style={{paddingTop: "3px", marginRight: "6px"}} icon={faUser}/>
+                            Account information
+                        </div>
+                    }
+                    panelContent={
+                        <div>
+                            <div>Ethereum address : 0x627306090abab3a6e1400e9345bc60c78a8bef57</div>
+                            <div>Ether balance : 100 </div>
+                        </div>
+                    }
+                />
+                <AnnotatedSection
+                    annotationContent={
+                        <div>
+                            <FontAwesomeIcon fixedWidth style={{paddingTop: "3px", marginRight: "6px"}}
+                                             icon={faCertificate}/>
+                            Certifications
+                        </div>
+                    }
+                    panelContent={
+                        <div>
+                            <table className="table table-sm mb-0 noHeaderLine">
+                                <tbody>
+                                {
+                                    // displays all available certifications
+                                    this.state.availableCertifications && this.state.availableCertifications.length > 0 ?
+                                        this.state.availableCertifications.map((certification, index) =>
+                                            <tr key={index}>
+                                                <td style={{width: '45px'}}>
+                                                    {
+                                                        certification.imageUrl && certification.imageUrl.length > 0 ?
+                                                            <img src={certification.imageUrl} alt={certification.name}
+                                                                 style={{maxWidth: '27px', maxHeight: '27px'}}/>
+                                                            :
+                                                            <div style={{
+                                                                width: '27px',
+                                                                height: '27px',
+                                                                background: '#c4c4c4'
+                                                            }}>&nbsp;</div>
+                                                    }
+                                                </td>
+                                                <td>{certification.name}</td>
+                                            </tr>
+                                        )
+                                        :
+                                        <tr>
+                                            <td>No certifications created yet.</td>
+                                        </tr>
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    }
+                />
+                {this.state.isGod &&
+                <UpdateGodUser/>
+                }
+            </div>
+        );
+    }
+}
+
+// function mapStateToProps(state) {
+//     return {
+//         web3: state.reducer.web3,
+//         passageInstance: state.reducer.passageInstance,
+//         web3Accounts: state.reducer.web3Accounts,
+//         userAddress: state.reducer.userAddress
+//     };
+// }
+
+export default Account;
+
